a7309e2b1ef3caf54de6af592c438ae3bfe7dd56
stevan06v <stevanvlajic5@gmail.com>
2024-12-10 14:06:02 +0100

    feat: added signup

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -36,8 +36,10 @@
             @if($user = auth()->user())
                 <x-menu-separator/>
 
-                <x-list-item :item="$user" value="name" sub-value="email" no-separator no-hover
+                <x-list-item :item="$user" value="name" sub-value="nickname" no-separator no-hover
                              class="-mx-2 !-my-2 rounded">
+
+
                     <x-slot:actions>
                         <x-button icon="o-power" class="btn-circle btn-ghost btn-xs" tooltip-left="logoff"
                                   no-wire-navigate link="/logout"/>
--- resources/views/livewire/auth/signup.blade.php
+++ resources/views/livewire/auth/signup.blade.php
@@ -1,24 +1,52 @@
 <?php
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Livewire\Volt\Component;
 use Livewire\Attributes\Layout;
 
 
 new class extends Component {
-    //
+
+    public ?string $nickname = null;
+    public ?string $email = null;
+    public ?string $password = null;
+    public ?string $password_confirmation = null;
+
+    public function save()
+    {
+        $validatedData = $this->validate([
+            'nickname' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:8|confirmed',
+        ]);
+
+        $user = User::create([
+            'nickname' => $this->nickname,
+            'email' => $this->email,
+            'password' => Hash::make($this->password)
+        ]);
+
+        Auth::login($user);
+
+        return redirect('/lists')->with('success', 'Account created successfully!');
+    }
+
 }; ?>
 
 <div class="max-w-sm lg:ml-40">
     <x-header title="Sign Up" subtitle="Create a new account." separator/>
     <x-form wire:submit="login">
-        <x-input label="Nickname" value="stevan06v" icon="o-envelope" inline/>
-        <x-input label="E-mail" value="random@random.com" icon="o-envelope" inline email/>
-        <x-input label="Password" value="random" type="password" icon="o-key" inline/>
-        <x-input label="Retype Password" value="random" type="password" icon="o-key" inline/>
+        <x-input label="Nickname" wire:model="nickname" value="stevan06v" icon="o-envelope" inline/>
+        <x-input label="E-mail" wire:model="email" value="random@random.com" icon="o-envelope" inline email/>
+        <x-input label="Password" wire:model="password" value="random" type="password" icon="o-key" inline/>
+        <x-input label="Retype Password" wire:model="password_confirmation" value="random" type="password" icon="o-key"
+                 inline/>
 
         <x-slot:actions>
             <x-button label="Login" link="/login" icon="o-paper-airplane" class="btn-primary" spinner="login"/>
-            <x-button label="Sign up" type="submit" icon="fas.user-plus" class="btn-primary" spinner="login"/>
+            <x-button label="Sign up" wire:click="save()" icon="fas.user-plus" class="btn-primary" spinner="signup"/>
         </x-slot:actions>
     </x-form>
 </div>
